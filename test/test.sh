22f676581ccf7871803687b7ff888e646a5e02a8
Hao Xu <xuh@cs.unc.edu>
2020-01-03 15:15:25 -0500

    update

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose
+chmod +x docker-compose
 MONGO_INITDB_ROOT_PASSWORD=example MONGO_NON_ROOT_PASSWORD=collection JWT_SECRET=secret ./docker-compose --env-file test/env -f docker-compose.yml -f test/docker-compose.yml up --build -V --exit-code-from txrouter-test
