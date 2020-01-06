ceac1eef48d039d631987dfb8cfeb950a7072fe3
Kimberly Robasky <krobasky@gmail.com>
2020-01-06 13:07:26 -0500

    fixes RENCI/pds#124, fixes RENCI/pds#125

--- test/test.sh
+++ test/test.sh
@@ -5,4 +5,4 @@
 # and also make variables available to test code in python scripts
 export $(sed -e 's/=\(.*\)/="\1/g' -e 's/$/"/g' test/env.docker |grep -v "^#"| xargs)
 
-MONGO_INITDB_ROOT_PASSWORD=example MONGO_NON_ROOT_PASSWORD=collection JWT_SECRET=secret ./docker-compose -f docker-compose.yml -f test/docker-compose.yml up --build -V --exit-code-from txrouter-test
+MONGO_INITDB_ROOT_PASSWORD=example MONGO_NON_ROOT_PASSWORD=collection JWT_SECRET=secret docker-compose -f docker-compose.yml -f test/docker-compose.yml up --build -V --exit-code-from txrouter-test
