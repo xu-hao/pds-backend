134d274af9b240ae4c0a1b8f710bd65938b287c7
krobasky <krobasky@gmail.com>
2020-01-08 02:03:20 -0500

    added env.TAG to aid RM, extracted environmentals to file to aid deployment

--- test/test.sh
+++ test/test.sh
@@ -5,4 +5,20 @@
 # and also make variables available to test code in python scripts
 export $(sed -e 's/=\(.*\)/="\1/g' -e 's/$/"/g' test/env.docker |grep -v "^#"| xargs)
 
+# get tag from this commit.
+# if there's no tag, use 'unstable' to tag the container on commits to master
+TX_TAG=`git describe --exact-match --tags $(git log -n1 --pretty='%h')  2> /dev/null; `
+if [ "${TX_TAG}" == "" ] ; then TX_TAG='unstable'; fi
+echo "TX_TAG=${TX_TAG}" > env.TAG
+
+export TX_TAG
+
+# note that the environmenal variables set above
+# will override any .env variables
+# setting them explicitly here avoids any ambiguity
+
 MONGO_INITDB_ROOT_PASSWORD=example MONGO_NON_ROOT_PASSWORD=collection JWT_SECRET=secret docker-compose -f docker-compose.yml -f test/docker-compose.yml up --build -V --exit-code-from txrouter-test
+
+# save info on how docker containers were configured
+MONGO_INITDB_ROOT_PASSWORD=example MONGO_NON_ROOT_PASSWORD=collection JWT_SECRET=secret docker-compose -f docker-compose.yml -f test/docker-compose.yml config > test/config.out
+
